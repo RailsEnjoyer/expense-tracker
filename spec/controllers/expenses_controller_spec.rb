e409315d4b8d600e1e57c4c16f9d10d462a44c89
railsnjoyer <railsenjoyer@gmail.com>
2025-04-18 21:19:04 +0200

    added tests for expense controller

--- spec/controllers/expenses_controller_spec.rb
+++ spec/controllers/expenses_controller_spec.rb
@@ -1,2 +1,16 @@
 require 'rails_helper'
 
+RSpec.describe "Expenses", type: :request do 
+  let(:user){ create(:user) }
+  let(:valid_params){ attributes_for(:expense) }
+  let(:invalid_params){ attributes_for(:expense, title: nil) }
+
+  before do 
+    post login_path, params: { email: user.email, password: user.password }
+  end
+
+  describe "GET /expenses" do 
+    let!(:expenses) { create_list(:expense, 3, user: user) }
+    #TODO complete test logic
+  end
+end
