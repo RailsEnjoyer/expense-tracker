ebb6e9061d98c4adf3cb76d8516fc89f8f8028c9
railsnjoyer <railsenjoyer@gmail.com>
2025-04-27 14:46:59 +0200

    added shared examples for form

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -7,3 +7,19 @@ shared_examples "redirects to login" do |http_method, path_proc|
     expect(response.body).to include("Join us!")
   end
 end
+
+shared_examples "form with preserved fields" do
+  it "re-renders form with preserved fields" do
+    expect(page).to have_content("Create new expense")
+
+    expect(page).to have_field('Title', with: expected_title)
+    expect(page).to have_field('Amount', with: expected_amount)
+
+    if expected_date.present?
+      expect(page).to have_field('Date of spent', with: expected_date)
+    else
+      expect(page).to have_field('Date of spent')
+      expect(find('input[name="expense[spent_on]"]').value).to be_nil
+    end
+  end
+end
